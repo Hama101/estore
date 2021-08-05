2dcb41636c929d39c2b597b85d3890e2d349717f
hamdi Mohamed <hamdi_mohamed2018@hotmail.com>
2021-08-05 19:23:30 +0100

    setting admin filters

--- products/admin.py
+++ products/admin.py
@@ -9,6 +9,35 @@ admin.site.register(Category , CategoryAdmin)
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name','cover','price' ,'category','mark','quantity','created_at')
+    list_filter = [
+            "category",
+            "mark",
+            "quantity",
+            "SKU",
+            "price",
+            "created_at"
+    ]
+    search_fields = (
+        "category__title",
+        "mark__name",
+        "name",
+        "quantity",
+        "SKU",
+        "price",
+    )
+    def get_search_results(self, request, queryset, search_term):
+        queryset, use_distinct = super().get_search_results(request, queryset, search_term)
+
+        try:
+            min_price, max_price = search_term.split('-')
+        except ValueError:
+            pass
+        else:
+            queryset |= self.model.objects.filter(price__gte=min_price, price__lte=max_price)
+            queryset |= self.model.objects.filter(quantity__gte=min_price, quantity__lte=max_price)
+        return queryset, use_distinct
+
+
 admin.site.register(Product , ProductAdmin)
 
 
--- users/admin.py
+++ users/admin.py
@@ -4,5 +4,19 @@ from .models import *
 
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ( 'user','name','avatar' , 'cover' , 'is_buyer')
-    
+    list_filter = [
+            "type",
+            "is_buyer",
+            "created_at"
+    ]
+    search_fields = (
+        "user__username",
+        "name",
+        "email",
+        "phone",
+        "secondPhoneNumber",
+        "postcode",
+    )
+
+
 admin.site.register(Profile , ProfileAdmin)
